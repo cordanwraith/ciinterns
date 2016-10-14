60451e7b2cacc4fbd185cbb097446972513c23d3
Eli Iannuzzo <eli.iannuzzo@ad.aie.edu>
2016-10-14 16:00:50 +1100

    Add object info, it holds a name, description, and adds the object its attached too, to the list of objects

---
+++ ProjectFiles/Cultural Infusion/Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInfo : MonoBehaviour
+{
+    public string m_name;
+    public string m_description;
+
+    void Start()
+    {
+        ObjectManager.AddObject(gameObject);
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
