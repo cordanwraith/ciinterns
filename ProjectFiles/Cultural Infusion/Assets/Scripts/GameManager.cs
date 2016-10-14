bd51efc2047751274d022bcf04af182b24fe63a1
Eli Iannuzzo <eli.iannuzzo@ad.aie.edu>
2016-10-14 16:00:02 +1100

    Initiates the object pool creation

---
+++ ProjectFiles/Cultural Infusion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    
+	void Start ()
+    {
+        ObjectManager.SetInitialPool(3);
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
