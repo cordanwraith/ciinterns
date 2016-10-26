272ece577963dbe08b61aa5cd0c723ff4f2522ad
Sean Volders <selor1@gmail.com>
2016-10-26 19:38:43 +1100

    Some basic polish on the camera touch-input controls

--- ProjectFiles/Cultural Infusion/Assets/Scripts/CameraController.cs
+++ ProjectFiles/Cultural Infusion/Assets/Scripts/CameraController.cs
@@ -11,8 +11,8 @@ public class CameraController : MonoBehaviour
     private bool doingZoom;
 
     //min and max distances for zooming
-    private float maxDistance = 24;
-    private float minDistance = 6;
+    private float maxDistance = 40;
+    private float minDistance = 10;
 
     void Start()
     {
@@ -45,10 +45,14 @@ public class CameraController : MonoBehaviour
                     (m_lastTouchInput[0].position - m_lastTouchInput[1].position).magnitude) * 0.5f;
                     Vector3 newPos = m_camera.transform.position + m_camera.transform.forward * z;
 
+                    Debug.Log("New pos :" + newPos);
+
                     if (newPos.magnitude < maxDistance
                         && newPos.magnitude > minDistance)
                     {
-                        m_camera.transform.position = newPos;
+                        //stops going through the floor on large movements below minimum
+                        if (Vector3.Dot(newPos, Vector3.zero) > 0)
+                            m_camera.transform.position = newPos;
                     }
                 }
                 else
@@ -62,9 +66,10 @@ public class CameraController : MonoBehaviour
             }
 
             //do rotation
-            if (t[0].deltaPosition.magnitude > 0)
+            if (t[0].deltaPosition.magnitude > 1.5f
+                && !doingZoom)
             {
-
+                Debug.Log("\tDelta Pos :" + t[0].deltaPosition);
                 Vector3 rotateAround = new Vector3(0, m_camera.transform.position.y, 0);
                 int dir = t[0].deltaPosition.x < 0 ? -1 : 1;
                 m_camera.transform.RotateAround(rotateAround, Vector3.up, t[0].deltaPosition.magnitude * dir);
