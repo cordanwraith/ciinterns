7f520daade104ae484578b426f31322a049ce9d0
IT <IT@PAN.local>
2016-11-10 14:28:27 +1100

    Fixed UI

--- ProjectFiles/Cultural Infusion/Assets/GUIButton.cs
+++ ProjectFiles/Cultural Infusion/Assets/GUIButton.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUIButton : MonoBehaviour
 {
     private const float m_endPos = 777;
     private const float m_startPos = 40;
 
-    [SerializeField]
     Vector3 _destinationPos = Vector3.zero, _startPos = Vector3.zero;
     private float m_currTransitionDur = 0;
     private float m_transitionLength   = 1;
@@ -23,7 +23,9 @@ public class GUIButton : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        _startPos = transform.localPosition;
+        _startPos = transform.position;
+        _destinationPos.y = transform.position.y;
+        _destinationPos.x = ((RectTransform)transform.FindChild("BackgroundImage")).rect.width * transform.parent.localScale.x * 1.05f;
         m_currentState = state.closed;
     }
 
@@ -40,7 +42,7 @@ public class GUIButton : MonoBehaviour
                 {
                     m_currTransitionDur += Time.deltaTime;
                     float transitionPercentage = m_currTransitionDur / m_transitionLength;
-                    transform.localPosition = Vector3.Lerp(_startPos, _destinationPos, transitionPercentage);
+                    transform.position = Vector3.Lerp(_startPos, _destinationPos, transitionPercentage);
                     Debug.Log("<b>TransPer</b> " + transitionPercentage.ToString());
                     if (transitionPercentage > 1)
                     {
@@ -54,10 +56,10 @@ public class GUIButton : MonoBehaviour
                 {
                     m_currTransitionDur += Time.deltaTime;
                     float transitionPercentage = m_currTransitionDur / m_transitionLength;
-                    transform.localPosition = Vector3.Lerp(_destinationPos, _startPos, transitionPercentage);
+                    transform.position = Vector3.Lerp(_destinationPos, _startPos, transitionPercentage);
                     Debug.Log("<b>TransPer</b> " + transitionPercentage.ToString());
-
-                    if (m_transitionLength > 1)
+                    
+                    if (transitionPercentage > 1)
                     {
                         m_currTransitionDur = 0;
                         m_currentState = state.closed;
